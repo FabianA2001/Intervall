1dd4cf2f9ddbb79e1f5b9c7b8113eb4d605e71f8
Fabian A <>
2025-10-09 15:44:44 +0200

    init

---
+++ app/src/main/java/com/intervall/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.intervall
+
+import android.app.PendingIntent
+import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Rect
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import android.util.Rational
+import android.widget.VideoView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toAndroidRect
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.net.toUri
+import com.intervall.ui.theme.IntervallTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    class MyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            println("Clicked on PIP action")
+        }
+    }
+
+    private val isPipSupported by lazy {
+        packageManager.hasSystemFeature(
+            PackageManager.FEATURE_PICTURE_IN_PICTURE
+        )
+    }
+
+    private var videoViewBounds = Rect()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntervallTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    AndroidView(
+                        factory = {
+                            VideoView(it, null).apply {
+                                setVideoURI("android.resource://$packageName/${R.raw.sample}".toUri())
+                                start()
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .onGloballyPositioned {
+                                videoViewBounds = it.boundsInWindow().toAndroidRect()
+                            }
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = {
+                            if (isPipSupported) {
+                                updatedPipParams()?.let { params ->
+                                    enterPictureInPictureMode(params)
+
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        Text("Play Info")
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun updatedPipParams(): PictureInPictureParams? {
+        return PictureInPictureParams.Builder()
+            .setSourceRectHint(videoViewBounds)
+            .setAspectRatio(Rational(16, 9))
+            .setActions(
+                listOf(
+                    RemoteAction(
+                        Icon.createWithResource(
+                            applicationContext,
+                            R.drawable.ic_android_black_24dp
+                        ),
+                        "Baby changing station",
+                        "Baby changing station",
+                        PendingIntent.getBroadcast(
+                            applicationContext,
+                            0,
+                            Intent(applicationContext, MyReceiver::class.java),
+                            PendingIntent.FLAG_IMMUTABLE
+                        )
+                    )
+                )
+            )
+            .build()
+    }
+}
